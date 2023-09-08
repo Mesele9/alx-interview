37657dee9b2205172ee9925179325615db71b740
Mesele Assefa <getmesy@gmail.com>
2023-09-08 02:08:09 +0000

    push task

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" 0-prime_game.py """
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def isWinner(x, nums):
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return 'Maria'
+    elif ben_wins > maria_wins:
+        return 'Ben'
+    else:
+        return None
