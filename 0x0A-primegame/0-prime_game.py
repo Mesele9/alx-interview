4ed0aa59f180a2fff6bb1d9466ed372597bfc8cd
Mesele Assefa <getmesy@gmail.com>
2023-09-08 02:23:41 +0000

    correction

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,12 +4,13 @@
 
 def is_prime(num):
     """ check for prime number"""
-    if num < 2:
+    if num == 0 or num == 1 or (num % 2 == 0 and num > 2):
         return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+    else:
+        for i in range(3, int(num ** 0.5) + 2):
+            if num % i == 0:
+                return False
+        return True
 
 
 def isWinner(x, nums):
